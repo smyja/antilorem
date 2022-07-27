720b8399c94b41b644281cc80866d1c3c9eb8816
Smyja <akposlive59@gmail.com>
2022-07-28 00:30:23 +0100

    upd

--- src/helpers/api.jsx
+++ src/helpers/api.jsx
@@ -6,12 +6,12 @@ console.log(process.env);
 export const api = {
   auth: {
     login: `${baseURL}/dj-rest-auth/login/`,
-    register:`${baseURL}/dj-rest-auth/registration/`,
-    logout: `${baseURL}/dj-rest-auth/logout/`
+    register: `${baseURL}/dj-rest-auth/registration/`,
+    logout: `${baseURL}/dj-rest-auth/logout/`,
   },
 
   posts: {
     list: `${baseURL}/api/blog/posts/`,
-    create: `${baseURL}/api/v1/post/`,
+    create: `${baseURL}/api/v1/post`,
   },
 };
